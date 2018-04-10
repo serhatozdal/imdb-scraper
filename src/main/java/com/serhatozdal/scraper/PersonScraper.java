c55288fac8046315b086a53bde32d14fd22d6a7b
serhatozdal <serhatozdal@gmail.com>
2018-04-10 17:56:14 +0300

    preparing person scraper

--- src/main/java/com/serhatozdal/scraper/PersonScraper.java
+++ src/main/java/com/serhatozdal/scraper/PersonScraper.java
@@ -1,11 +1,24 @@
 package com.serhatozdal.scraper;
 
+import com.serhatozdal.scraper.http.Url;
 import com.serhatozdal.scraper.model.Content;
+import com.serhatozdal.scraper.model.Person;
 
 public class PersonScraper extends Scraper {
 
     @Override
     public Content findById(String id) {
-        return null;
+
+        final Person person = new Person();
+
+        return person;
+    }
+
+    private void parseMainPage(String id, Person person) {
+
+        String imdbUrl = "http://www.imdb.com/title/" + id;
+
+        Url url = new Url();
+        String html = url.fetchHtml(imdbUrl);
     }
 }
--- src/main/java/com/serhatozdal/scraper/model/Person.java
+++ src/main/java/com/serhatozdal/scraper/model/Person.java
@@ -1,16 +1,6 @@
 package com.serhatozdal.scraper.model;
 
-import java.io.Serializable;
+public class Person extends Content {
 
-public class Person implements Serializable {
 
-    private Boolean personFound = false;
-
-    public Boolean isPersonFound() {
-        return personFound;
-    }
-
-    public void setPersonFound(Boolean personFound) {
-        this.personFound = personFound;
-    }
 }
